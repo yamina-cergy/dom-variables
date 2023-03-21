a6a44064344ae4103934d958bbd6abd072aa058c
Yamina <yham2575@gmail.com>
2023-03-21 17:04:12 +0100

    Initial commit

---
+++ exercice2103Yamina.js
@@ -0,0 +1,24 @@
+//exo1
+var age = 25;
+//exo2
+var nom = "Jean";
+//exo3
+var somme = 5 + 2;
+//exo4
+var chaine = "il fait beau.";
+var result = chaine.toUpperCase();
+//exo5
+let tableau = [1,2,3,4,5];
+var result = tableau.length;
+//exo6
+var objet = {nom : Jean , age : 25}
+console.log(objet.nom);
+console.log(objet.age);
+//exo7
+var nombre = 1,33265366
+var result = nombre.toFixed(2);
+//exo8
+var date = new Date
+let month = date.getMonth()
+//exo9
+var tableau2 = []
